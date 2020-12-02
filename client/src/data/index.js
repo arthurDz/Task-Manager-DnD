8008049b666ae59bd6edfe4194d01869dd843072
unknown <shashankgupta199904@gmail.com>
2020-12-02 18:13:45 +0530

    final commit

---
+++ client/src/data/index.js
@@ -0,0 +1,46 @@
+const data = [{
+    id: 1,
+    icon: "⭕️",
+    status: "open",
+    title: "Angular Class",
+    content: "Watch a tutorial on AngularJs"
+}, {
+    id: 2,
+    icon: "⭕️",
+    status: "open",
+    title: "Purchase present",
+    content: "Get an anniversary gift"
+}, {
+    id: 3,
+    icon: "⭕️",
+    status: "open",
+    title: "Submit an assignment",
+    content: "Submit an assignment on mobile networking"
+}, {
+    id: 4,
+    icon: "⭕️",
+    status: "open",
+    title: "Daily reading",
+    content: "Finish reading Intro to ReactJs"
+}];
+
+const statuses = [{
+    status: "open",
+    icon: "⭕️",
+    color: "#EB5A46"
+}, {
+    status: "in progress",
+    icon: "🔆️",
+    color: "#00C2E0"
+}, {
+    status: "in review",
+    icon: "📝",
+    color: "#C377E0"
+}, {
+    status: "done",
+    icon: "✅",
+    color: "#3981DE"
+}];
+
+
+export { data, statuses };
